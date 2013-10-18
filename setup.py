ed763958580d47d3489e640abd33b3833f2fbbc2
Jonathan Lange <jml@mumak.net>
2013-10-18 17:10:36 +0100

    Explain the version pinning

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     packages=['edn'],
     package_data={'edn': ['edn.parsley']},
     install_requires=[
+        # iso8601 0.1.5 introduces a timezone parsing bug.
+        # https://bitbucket.org/micktwomey/pyiso8601/issue/8/015-parses-negative-timezones-incorrectly
         'iso8601==0.1.4',
         'parsley>=1.2',
         'perfidy',
